b4fc2d5f82513eee6408e89c7b98ea4e641f6258
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-13 12:20:32 +0200

    Leo Enti

--- Assets/Game/Scripts/DeactivateCollider.cs
+++ Assets/Game/Scripts/DeactivateCollider.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class DeactivateCollider : MonoBehaviour
 {
     public Transform referenceObject;
+
     public float thresholdY;
     private CapsuleCollider2D coll2D;
+    public float posYbike;
 
     private void Start()
     {
-        coll2D = GetComponent<CapsuleCollider2D>();
+        coll2D = GetComponent<CapsuleCollider2D>();        
     }
 
     private void Update()
     {
-        if (transform.position.y - referenceObject.position.y < thresholdY)
+        posYbike = transform.position.y;
+        if (posYbike - referenceObject.position.y < thresholdY)
         {
             coll2D.enabled = false;
         }
-        if (transform.position.y - referenceObject.position.y > thresholdY)
+        if (posYbike - referenceObject.position.y > thresholdY)
         {
             coll2D.enabled = true;
         }
--- Assets/Game/Scripts/DisableColliderOnJump.cs
+++ Assets/Game/Scripts/DisableColliderOnJump.cs
@@ -5,31 +5,25 @@ using UnityEngine;
 public class DisableColliderOnJump : MonoBehaviour
 {
     private CapsuleCollider2D coll2D;
+    public Transform referenceObject;
     public float posY;
+    public float posYbike;
 
     void Start()
     {
-        // Obtenemos el componente Collider2D del objeto
-        coll2D = GetComponent<CapsuleCollider2D>();
-        // Guardamos la posición inicial del personaje
-        posY = transform.position.y;
+        coll2D = GetComponent<CapsuleCollider2D>();              
     }
 
     void Update()
     {
-        // Si el personaje está saltando (su posición en Y es mayor que la anterior), desactivamos el collider
-        if (transform.position.y > posY)
+        posYbike = transform.position.y;
+        if (referenceObject.transform.position.y < posYbike)
         {
             coll2D.enabled = false;
         }
-        if (transform.position.y < posY)
+        if (referenceObject.transform.position.y > posYbike)
         {
             coll2D.enabled = true;
-        }
-
-        // Guardamos la posición actual del personaje para compararla en la siguiente actualización del frame
-        //previousYPosition = transform.position.y;
+        }        
     }
 }
-
-
